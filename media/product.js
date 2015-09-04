16694c8f1a0a1a6d8444651eb5d36d74f2ccd1f8
arohy <a.kosheverov@insales.ru>
2015-09-04 17:39:46 +0300

    2015.09.04 fixes

--- media/product.js
+++ media/product.js
@@ -46,7 +46,6 @@ $(function(){
 
   // обновляем инфу, после смены модификации
   Events( 'onProductOption_Changed' ).subscribe( function( $data ){
-    console.log( 'onProductOption_Changed', $data );
     var
       variant           = $data.variant,
       $js_buy           = $( '.product-buy' ),
@@ -67,29 +66,63 @@ $(function(){
 
     if( variant && variant.available == true ){
       // selected a valid variant
-
-      $js_buy.val( 'Купить' ).removeClass( 'button--disabled' );
-      $js_price_current.html( InSales.formatMoney( variant.price ) );
+      $js_price_current
+        .removeClass( 'product-presence_field--sell_off' )
+        .html( InSales.formatMoney( variant.price ) );
       $js_price_old.html( InSales.formatMoney( variant.old_price ) );
       $js_presence.html( 'Есть в наличии' );
 
-    }else{
-      // variant doesn't exist
-
-      $js_buy.val( 'Под заказ' ).addClass( 'button--disabled' );
-
-      if( variant ){
-        $js_presence.html( 'Распродано' );
-        $js_price_current.html( InSales.formatMoney( variant.price ) );
-        $js_price_old.html( InSales.formatMoney( variant.old_price ) );
-      }else{
-        $js_presence.html( '' );
-        $js_price_current.html( 'Модификация отсутсвует' );
-        $js_price_old.html( '' );
+      if( Site.product.buy_button == 'block' ){
+        $js_buy
+          .html( '<i class="fa fa-shopping-cart" /> В корзину' )
+          .removeClass( 'button--disabled' )
+          .prop( 'disabled', false );
+      }
+      else if( Site.product.buy_button == 'buy' ){
+        $js_buy
+          .html( '<i class="fa fa-shopping-cart" /> В корзину' );
       };
 
+    }else{
+      // variant doesn't exist
+      if( Site.product.buy_button == 'block' ){
+        $js_buy
+          .html( '<i class="fa fa-ban" /> Распродано' )
+          .addClass( 'button--disabled' )
+          .prop( 'disabled', true );
+      }
+      else if( Site.product.buy_button == 'buy' ){
+        $js_buy
+          .html( '<i class="fa fa-shopping-cart" /> Под заказ' );
+      };
+
+      if( variant ){
+        $js_presence
+          .addClass( 'product-presence_field--sell_off' )
+          .html( 'Распродано' );
+        $js_price_current
+          .html( InSales.formatMoney( variant.price ) );
+        $js_price_old
+          .html( InSales.formatMoney( variant.old_price ) );
+      }else{
+        $js_presence
+          .html( '' );
+        $js_price_current
+          .html( 'Модификация отсутсвует' );
+        $js_price_old
+          .html( '' );
+      };
     };
   });
 
-  $( '.quantity' ).quantity();
+  $( '.quantity' )
+    .quantity();
+
+  Events( 'onBuyButton_Active' ).subscribe( function( $data ){
+    //console.log( 'onBuyButton_Active: ', $data );
+  });
+
+  Events( 'onBuyButton_Inactive' ).subscribe( function( $data ){
+    //console.log( 'onBuyButton_Inactive: ', $data );
+  });
 });
--- media/ui.js
+++ media/ui.js
@@ -3,20 +3,61 @@
 // ===========================================================
 
 $(function(){
-  $( document ).on( 'click touchstart', '.js-panel-link', function(){
-    $( 'body' ).css( 'overflow', 'hidden' );
-    $( '.js-mobile_panel' ).show();
-    $( '.overlay' ).show();
+  // search_widget
+  $( '.js-search_widget-wrapper' ).hide();
+
+  $( document ).on( 'click', '.js-search_widget-toggler', function( e ){
+    e.preventDefault();
+
+    $( '.js-search_widget-wrapper' )
+      .toggle();
+  })
+
+  // ui для панели
+  $( '.js-panel-link' ).on( 'click', function(){
+    var
+      params = getParams( $(this) ),
+      $unit  = $(this)
+
+    // flag;
+    params.panel = true;
+
+    $( 'body' )
+      //.css( 'overflow', 'hidden' )
+      .addClass( 'lock_scroll' )
+      .append( '<div class="overlay" />');
+
+    $( params.target ).show();
+
+    setParams( $( '.overlay' ), params );
+
+    window.setTimeout( function(){
+      $unit
+        .toggleClass( 'js-panel-close' )
+    }, 200 );
   });
 
-  $( document ).on( 'click touchstart', '.js-panel-close, .overlay', function(){
-    $( '.js-mobile_panel' ).hide();
-    $( '.overlay' ).hide();
-    $( 'body' ).css( 'overflow', 'initial' );
+  $( document ).on( 'click touchstart', '.js-panel-close, .overlay', function( e ){
+    e.preventDefault();
+    var
+      params = getParams( $( '.overlay' ) );
+
+    if( params.panel ){
+      $( params.target ).hide();
+      $( '.overlay' ).remove();
+      $( 'body' )
+        .removeClass( 'lock_scroll' )
+        //.css( 'overflow', 'initial' );
+
+      $( '.js-panel-link' )
+        .removeClass( 'js-panel-close' );
+    }
   });
 
-  styleSelect( 'select' );
+  // стилизация селектов
+  styleSelect( 'select.js-styled' );
 
+  // прокуртка в начало
   $( '.js-scroll_to_top' ).on( 'click', function(){
     $( 'body' ).animate({
       scrollTop: 0
@@ -29,4 +70,84 @@ $(function(){
 
     console.log( InSales.Render( $data, 'search', 'ajax' ) );
   });
+
+  // улучшайзинг стандартной обратной связи
+  if( Site.template == 'page' ){
+    var
+      $form = $( '#feedback_form');
+
+    if( $form.is( 'form' ) ){
+      var
+        table     = $form.find( 'table' ),
+        notice    = table.find( 'small' ),
+        button    = table.find( '#feedback_commit' ),
+        textarea  = table.find( 'textarea' ),
+        email     = table.find( 'input[name="feedback[from]"]' );
+
+      $form
+        .removeAttr( 'style' );
+
+      table
+        .find( 'td' )
+          .removeAttr( 'style' );
+
+      textarea
+        .removeAttr( 'style' )
+        .addClass('input-field')
+        
+        .parents( 'p:first' )
+          .addClass( 'input input--required' )
+          .append('<div class="input-notice notice notice--danger">Заполните поле</div>')
+            .find( 'label' )
+              .addClass( 'input-label' );
+
+      email
+        .removeAttr( 'size' )
+        .attr( 'type', 'email' )
+        
+        .addClass('input-field')
+        .parents( 'p:first' )
+          .addClass( 'input input--required' )
+          .append('<div class="input-notice notice notice--danger">E-mail неправильно заполнен</div>')
+            .find( 'label' )
+              .addClass( 'input-label' );
+
+      table
+        .removeAttr( 'style' )
+        .find( 'br')
+          .remove();
+
+      notice
+        .parent()
+          .html( notice.html() )
+          .addClass( 'notice notice--info' );
+
+      button
+        .addClass('button button--action mc-grid-12')
+        .on( 'click', function( e ){
+          e.preventDefault();
+          var
+            $message = {},
+            errors;
+
+          errors = checkForm( $form );
+
+          if( errors.length > 0 ){
+            return;
+          };
+
+          $message[ 'feedback[content]' ] = textarea.val();
+          $message[ 'feedback[from]' ] = email.val();
+
+          // отправка и красивости
+          InSales.sendMessage( $message )
+            .done( function( response ){
+              console.log( response );
+              if( response.status == 'ok' ){
+                showMessage( response.notice, 5000 );
+              };
+            });
+        });
+    };
+  };
 })
