4609298d996af7d4e3c6f0943e04f5f6e65c753a
arohy <a.kosheverov@insales.ru>
2015-09-22 16:22:56 +0300

    init max_template

---
+++ max_template/media/slider.js
@@ -0,0 +1,28 @@
+// ======================================================================================
+//                                         SLIDER
+// ======================================================================================
+
+$(function(){
+  $( '.js-slider' ).owlCarousel({
+    responsive: {
+      0: { items: 1 },
+      480: { items: 3 },
+      800: { items: 4 }
+    },
+    slideBy: 'page',
+  });
+
+  $( '.js-slider-left' ).on( 'click', function(){
+    $(this)
+      .parents( '.slider:first' )
+        .find( '.slider-container' )
+          .trigger( 'prev.owl.carousel' );
+  });
+
+  $( '.js-slider-right' ).on( 'click', function(){
+    $(this)
+      .parents( '.slider:first' )
+        .find( '.slider-container' )
+          .trigger( 'next.owl.carousel' );
+  });
+});
