eff3570ea4acc0f3426966bb4958efcbd2d25c38
arohy <a.kosheverov@insales.ru>
2015-10-21 17:26:15 +0300

    rebuild test

--- test/media/z.InSales.formValidation.js
+++ test/media/z.InSales.formValidation.js
@@ -86,12 +86,16 @@ $(function(){
 });
 
 // принимает jQuery-объект формы
-sendForm = function( $form, from, subject ){
+sendForm = function( options ){
+  //$form, from, subject
   var
+    $form    = options.form,
+    from     = options.from,
+    subject  = options.subject,
     errors   = checkForm( $form ),
     content  = '',
     $message = {},
-    $inputs  = $form.find( '.input-fields' );
+    $inputs  = $form.find( '.input-field' );
 
   // проверяем на ошибки
   if( errors.length > 0 ){
@@ -115,16 +119,22 @@ sendForm = function( $form, from, subject ){
     $message[ 'feedback[subject]' ] = subject;
   }
 
+  showPreloader();
+
   // возвращаем Deferred объект
-  return InSales.sendMessage( $message );
-  /*
+  InSales.sendMessage( $message )
     .done( function( response ){
       console.log( response );
       if( response.status == 'ok' ){
 
-        modal.close();
+        if( options.callback ){
+          options.callback();
+        }
+
         showMessage( response.notice );
       }
+    })
+    .always( function(){
+      hidePreloader();
     });
-    */
 };
--- test/media/z.InSales.ui.js
+++ test/media/z.InSales.ui.js
@@ -66,85 +66,80 @@ styleSelect = function( selector ){
 };
 $( function(){
   // улучшайзинг стандартной обратной связи
-  if( Site.template == 'page' ){
+  var
+    $form = $( '#feedback_form');
+
+  if( $form.is( 'form' ) ){
+    // находим все элементы для стилизации
     var
-      $form = $( '#feedback_form');
+      table     = $form.find( 'table' ),
+      notice    = $form.find( 'small' ),
+      button    = $form.find( '#feedback_commit' ),
+      textarea  = $form.find( 'textarea' ),
+      email     = $form.find( 'input[name="feedback[from]"]' );
 
-    if( $form.is( 'form' ) ){
-      var
-        table     = $form.find( 'table' ),
-        notice    = table.find( 'small' ),
-        button    = table.find( '#feedback_commit' ),
-        textarea  = table.find( 'textarea' ),
-        email     = table.find( 'input[name="feedback[from]"]' );
+    // сносис инлайновые стили
+    $form
+      .removeAttr( 'style' );
 
-      $form
+    table
+      .find( 'td' )
         .removeAttr( 'style' );
 
-      table
-        .find( 'td' )
-          .removeAttr( 'style' );
+    // стилизуем поле сообщения и его подсказку
+    textarea
+      .removeAttr( 'style' )
+      .attr( 'title', 'Сообщение' )
+      .addClass('input-field')
+      .parents( 'p:first' )
+        .addClass( 'input input--required' )
+        .append('<div class="input-notice notice notice--danger">Заполните поле</div>')
+          .find( 'label' )
+            .addClass( 'input-label' );
 
-      textarea
-        .removeAttr( 'style' )
-        .addClass('input-field')
-        
-        .parents( 'p:first' )
-          .addClass( 'input input--required' )
-          .append('<div class="input-notice notice notice--danger">Заполните поле</div>')
-            .find( 'label' )
-              .addClass( 'input-label' );
+    // стилизуем поле ввода email
+    email
+      .removeAttr( 'size' )
+      .attr({ type: 'email', title: "E-mail" })
+      .addClass('input-field')
+      .parents( 'p:first' )
+        .addClass( 'input input--required' )
+        .append('<div class="input-notice notice notice--danger">E-mail неправильно заполнен</div>')
+          .find( 'label' )
+            .addClass( 'input-label' );
 
-      email
-        .removeAttr( 'size' )
-        .attr( 'type', 'email' )
-        
-        .addClass('input-field')
-        .parents( 'p:first' )
-          .addClass( 'input input--required' )
-          .append('<div class="input-notice notice notice--danger">E-mail неправильно заполнен</div>')
-            .find( 'label' )
-              .addClass( 'input-label' );
+    // чистим таблицу
+    table
+      .removeAttr( 'style' )
+      .find( 'br' )
+        .remove();
 
-      table
-        .removeAttr( 'style' )
-        .find( 'br')
-          .remove();
+    // форматируем начальные подсказки
+    notice
+      .parent()
+        .html( notice.html() )
+        .addClass( 'notice notice--info' );
 
-      notice
-        .parent()
-          .html( notice.html() )
-          .addClass( 'notice notice--info' );
+    // стилизуем кнопку ( отправить )
+    // вешаем обработку
+    button
+      .addClass('button button--action mc-grid-12')
+      .on( 'click', function( e ){
+        e.preventDefault();
 
-      button
-        .addClass('button button--action mc-grid-12')
-        .on( 'click', function( e ){
-          e.preventDefault();
-          var
-            $message = {},
-            errors;
-
-          errors = checkForm( $form );
-
-          if( errors.length > 0 ){
-            return;
+        sendForm({
+          form: $form,
+          from: email.val(),
+          callback: function(){
+            $form
+              .find( '.input-field' )
+                .val( '' );
           }
-
-          $message[ 'feedback[content]' ] = textarea.val();
-          $message[ 'feedback[from]' ] = email.val();
-
-          // отправка и красивости
-          InSales.sendMessage( $message )
-            .done( function( response ){
-              console.log( response );
-              if( response.status == 'ok' ){
-                showMessage( response.notice, 5000 );
-              }
-            });
         });
-    }
+      });
   }
 });
+
 // =======================================================================
 //                              TABS
 // =======================================================================
