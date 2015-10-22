540f13c1001a13a18e3a7122b91bedfc2dd5d3e8
arohy <a.kosheverov@insales.ru>
2015-10-22 16:55:12 +0300

    slider: разделены скрипты исправлена верстка

---
+++ blocks/slider/z.slider.ui.js
@@ -0,0 +1,13 @@
+$( document ).on( 'click', '.js-slider-left', function(){
+  $(this)
+    .parents( '.slider:first' )
+      .find( '.slider-container' )
+        .trigger( 'prev.owl.carousel' );
+});
+
+$( document ).on( 'click', '.js-slider-right', function(){
+  $(this)
+    .parents( '.slider:first' )
+      .find( '.slider-container' )
+        .trigger( 'next.owl.carousel' );
+});
