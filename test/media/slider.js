f58312ef731fdd6e9598dd12adc9b65c401e18cf
arohy <a.kosheverov@insales.ru>
2015-10-22 16:47:25 +0300

    rebuild test

--- test/media/slider.js
+++ test/media/slider.js
@@ -6,25 +6,20 @@ $(function(){
   $( '.js-slider' ).owlCarousel({
     responsive: {
       0:    { items: 1 },
-      480:  { items: 3 },
-      640:  { items: 3 },
-      800:  { items: 4 },
-      1100: { items: 4 },
+      480:  { items: 3, slideBy: 3 },
+      640:  { items: 3, slideBy: 3 },
+      800:  { items: 4, slideBy: 4 },
+      1100: { items: 4, slideBy: 4 },
     },
-    slideBy: 'page',
   });
 
-  $( '.js-slider-left' ).on( 'click', function(){
-    $(this)
-      .parents( '.slider:first' )
-        .find( '.slider-container' )
-          .trigger( 'prev.owl.carousel' );
-  });
-
-  $( '.js-slider-right' ).on( 'click', function(){
-    $(this)
-      .parents( '.slider:first' )
-        .find( '.slider-container' )
-          .trigger( 'next.owl.carousel' );
+  $( '.js-slider--gallery' ).owlCarousel({
+    responsive: {
+      0:    { items: 1 },
+      480:  { items: 1 },
+      640:  { items: 1 },
+      800:  { items: 3, slideBy: 3 },
+      1100: { items: 3, slideBy: 3 },
+    },
   });
 });
--- test/media/z.InSales.ui.js
+++ test/media/z.InSales.ui.js
@@ -140,6 +140,20 @@ $( function(){
   }
 });
 
+$( document ).on( 'click', '.js-slider-left', function(){
+  $(this)
+    .parents( '.slider:first' )
+      .find( '.slider-container' )
+        .trigger( 'prev.owl.carousel' );
+});
+
+$( document ).on( 'click', '.js-slider-right', function(){
+  $(this)
+    .parents( '.slider:first' )
+      .find( '.slider-container' )
+        .trigger( 'next.owl.carousel' );
+});
+
 // =======================================================================
 //                              TABS
 // =======================================================================
